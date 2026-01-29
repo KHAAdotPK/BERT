31ef24e5099dd217032d6639618273d134a31076
KHAAdotPK <spysee.pk@gmail.com>
2026-01-29 15:00:34 +0500

    Added training data files.

---
+++ Implementation/lib/PULL.cmd
@@ -0,0 +1,9 @@
+git clone https://github.com/KHAAdotPK/ala_exception.git
+git clone https://github.com/KHAAdotPK/allocator.git
+git clone https://github.com/KHAAdotPK/String.git
+git clone https://github.com/KHAAdotPK/csv.git
+git clone https://github.com/KHAAdotPK/Numcy.git
+git clone https://github.com/KHAAdotPK/parser.git
+git clone https://github.com/KHAAdotPK/sundry.git
+git clone https://github.com/KHAAdotPK/Corpus.git
+git clone https://github.com/KHAAdotPK/read_write_weights.git
